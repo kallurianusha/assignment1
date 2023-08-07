c61cab7dcec7ae8fb3179306dd2d3e06fb66f576
kallurianusha <kallurianu13marolix@gmail.com>
2023-08-07 12:54:32 +0530

    third commit

---
+++ 2nd file.py
@@ -0,0 +1,127 @@
+#s = "Hello, World!"
+#s1 = "l"
+#new_string = s.replace(s1,"")
+#print(new_string)
+
+
+#s = "python coding"
+#s2 = "n"
+#new_string = s.replace(s2,"")
+#print(new_string)
+
+
+ 
+#s = "cyclic"
+#s2 == s[::-3]
+#string= s.ispalindrome(s)
+#if x:
+#    print("true")
+#else:
+#    print("false")
+
+#n = 101
+#s = str(n)
+#reverse = s[::-1]
+#if(s == reverse):
+#    print("yes it ispalindrome")
+#else:
+#    print("no its not palindrome")
+
+#n = 12345
+#seq = str(n)
+#if (seq == "".join(reveresed(seq))):
+#    print ("Yes it's Palindromic Number ")
+#else:
+#    print ("no it is not palindrome number ")
+
+
+#string = input(int("enter a string:"))
+#if(string == string[::-2]):
+#    print("it is palindrome")
+#else:
+#    print ("not a palindrome")
+
+#char = input("enter alphabets:")
+#vowels = ['a', 'e', 'i', 'o', 'u']
+#consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+#if char.lower() in vowels:
+#    print("it is vowel")
+#elif char.lower() in consonants:
+#    print("it is consonants")
+#else:
+#    print("it is not a letter")
+
+
+#string = "python,coder"
+#char_to_replace = " "
+#new_string = string.replace(char_to_replace, "-")
+#print(new_string)
+
+
+#string = input("enter a string:")
+ #   alphabets = 0
+#    digits = 0
+ #   special_characters = 0
+#    for character in string:
+#        if character.isalpha():
+#            alpha_count += 1
+#        elif character.isdigit():
+#            digit_count+= 1
+ #       else:
+#            special_characters += 1
+#print("alphabet count is:",alpha_ count)
+#print("digit count is:",digit_count)
+#print("special character count is:",special_character count)
+
+#s =input("enter any string with blank spaces:")
+#s1 = s.replace(" ","")
+#print("new string after removing blank spaces")
+#print(s1)
+
+
+#str = input("enter a string:")
+#sum = 0
+#for i in str:
+#    if i.isdigit():
+#        sum = sum+int(i)
+#print("sum=",sum)
+
+
+#s = input("enter any string:")
+#l =[]
+#for char in s:
+#    if char not in l:
+#        l.append(char)
+#output = " ".join(l)
+#print(output)
+
+# count = 0
+# s1 = "coding"
+# char = "i"
+#for i in s1:
+#    if i == char:
+#        count += 1
+#print(count)
+
+
+#s1 = input("enter strings:")
+#s2 = input("enter string:")
+#if len(str1)!=len (str2):
+#    print("not anagrams")
+#else:
+#if sorted(s1) == sorted(s2):
+#    print("the two strings are not anograms")
+#else:
+#    print("the two strings are anograms")
+  
+
+# s = input("enter some string to sort:")
+#    alpha = [] 
+#    num = [] 
+#    for i in str1: 
+#        if i.isalpha(): 
+#            alpha.append(i) 
+#        else: 
+#            num.append(i) 
+#   output = "".join(sorted(alphabets)+sorted(numeric))
+#    print(output)
